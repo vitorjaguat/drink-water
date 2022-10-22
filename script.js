9d6c1ef5df5991ab70f047fc21ac500e338032d7
Vitor Jaguat <vitorbutkus@gmail.com>
2022-10-22 18:09:40 -0300

    restructure language translation, add background

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const smallCups = document.querySelectorAll('.cup-small');
-const liters = document.getElementById('liters');
 const percentage = document.getElementById('percentage');
 const remained = document.getElementById('remained');
 
-upadateBigCup();
+let selected_lang = 'en';
+
+updateBigCup();
 
 smallCups.forEach((cup, idx) => { 
     cup.addEventListener('click', () => highlightCups(idx));
@@ -21,12 +22,13 @@ function highlightCups(idx) {
             cup.classList.remove('full');
         }
     })
-    upadateBigCup();
+    updateBigCup();
 }
 
-function upadateBigCup() {
+function updateBigCup() {
     const fullCups = document.querySelectorAll('.cup-small.full').length;
     const totalCups = smallCups.length;
+    const liters = document.getElementById('liters');
 
     if(fullCups === 0) {
         percentage.style.visibility = 'hidden';
@@ -42,7 +44,48 @@ function upadateBigCup() {
         remained.style.height = 0;
     } else {
         remained.style.visibility = 'visible';
-        liters.innerText = `${2 - (250 * fullCups / 1000)}L`
+        liters.innerText = `${2 - (250 * fullCups / 1000)}${liters.dataset[selected_lang]}`
     }
 
 }
+
+
+//Translation:
+// const btnEn = document.getElementById('btn-en');
+// const btnPt = document.getElementById('btn-pt');
+// const btnZh = document.getElementById('btn-zh');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        selected_lang = e.target.dataset.lang;
+        translateDataset(selected_lang);
+        updateBigCup();
+    })
+})
+
+function translateDataset(language) {
+    if(selected_lang === 'en') {
+        remained.innerHTML = '<span id="liters" data-zh="公升" data-pt="L" data-en="L"></span><small data-pt="Faltando" data-zh="还差">Remaining</small>'
+    }
+    if(selected_lang === 'zh') {
+        remained.innerHTML = '<small>还差</small><span id="liters" data-zh="公升" data-pt="L" data-en="L"></span>'
+    }
+    if(selected_lang === 'pt') {
+        remained.innerHTML = '<small>Faltando</small><span id="liters" data-zh="公升" data-pt="L" data-en="L"></span>'
+    }
+
+    const texts = document.querySelectorAll(`[data-${language}]`);
+    texts.forEach(text => text.textContent = text.dataset[language]);
+    updateBigCup();
+}
+
+//Translation buttons
+const transBtns = document.querySelectorAll('.btn');
+
+transBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        selected_lang = btn.dataset['lang'];
+        updateBigCup();
+    })
+})
